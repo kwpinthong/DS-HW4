bf3b557adb98d2f27be4aec3d805e4a467fb2cc3
icqcq39 <cute_cloudian@hotmail.com>
2016-10-15 19:04:06 +0700

    innitial add
    add starter code and doc.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+package hw4;
+
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        
+        //Node tree = constructTree1();
+        //tree.printTree();
+        
+        
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,29 @@
+package hw4;
+
+public class Node {
+
+    Node left;
+    Node right;
+    int data;
+
+    public Node(int data) {
+
+    }
+
+    public void printTree() {
+        // uncomment the following line (and fix the error)
+        // super.printTree(this);
+    }
+
+    public void printBFT() {
+        Queue q = new Queue(50);
+        System.out.print("BFT node sequence [ ");
+        System.out.println("]");
+    }
+
+    public void printDFT() { // PreOrder
+        Stack s = new Stack(50);
+        System.out.print("DFT node sequence [ ");
+        System.out.println("]");
+    }
+}
